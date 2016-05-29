77f69fc9202b2ba326195823fe3af565bc9048e6
Jules Jacobsen <jules.jacobsen@sanger.ac.uk>
2016-05-29 13:01:25 +0100

    Removed deprecated API call in GenericFrequency.

--- src/main/java/org/exomiser/vas/model/GenericFrequency.java
+++ src/main/java/org/exomiser/vas/model/GenericFrequency.java
@@ -1,5 +1,6 @@
 package org.exomiser.vas.model;
 
+import com.google.common.base.MoreObjects;
 import com.google.common.base.Objects;
 
 /**
@@ -41,7 +42,7 @@ public class GenericFrequency implements Frequency {
 
     @Override
     public String toString() {
-        return Objects.toStringHelper(this)
+        return MoreObjects.toStringHelper(this)
                 .add("source", source)
                 .add("frequency", frequency)
                 .toString();
