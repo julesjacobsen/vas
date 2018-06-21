15d581793f932df4db7fe471941b373f645b3e2e
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2018-06-21 12:16:55 +0100

    Remove pre-Spring Boot 2.0 configuration and use new application.properties options Add access log

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ VOLUME /data
 #ARG JAR_FILE
 #COPY ${JAR_FILE} app.jar
 ADD /target/vas-1.0-SNAPSHOT.jar app.jar
-ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom", "-Xms2G", "-Xmx4G", "-jar","app.jar"]
+ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom", "-Xms2G", "-Xmx4G", "-jar", "app.jar"]
--- src/main/java/org/exomiser/vas/config/ControllerConfig.java
+++ src/main/java/org/exomiser/vas/config/ControllerConfig.java
@@ -1,11 +1,9 @@
 package org.exomiser.vas.config;
 
-import io.undertow.UndertowOptions;
 import org.exomiser.vas.api.VariantAnnotationService;
 import org.h2.mvstore.MVStore;
 import org.monarchinitiative.exomiser.core.genome.VariantAnnotator;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,15 +12,6 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class ControllerConfig {
-
-    @Bean
-    UndertowServletWebServerFactory embeddedServletContainerFactory() {
-        UndertowServletWebServerFactory factory = new UndertowServletWebServerFactory();
-        factory.addBuilderCustomizers(
-                builder -> builder.setServerOption(UndertowOptions.ENABLE_HTTP2, true));
-        return factory;
-    }
-
 
     @Bean
     public VariantAnnotationService hg19VariantAnnotationService(@Qualifier("hg19variantAnnotator") VariantAnnotator variantAnnotator, @Qualifier("hg19mvStore") MVStore alleleMvStore) {
